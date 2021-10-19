dc5d87cbdd4ad148484295ac3155a563a47adcbb
successfulcode <56683358+successfulcode@users.noreply.github.com>
2021-10-19 23:31:27 +0300

    Created HeaderDict component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from 'antd';
+import { FC } from 'react';
+import { Button, Layout } from 'antd';
+import HeaderDict from './components/HeaderDict/HeaderDict'
 
-const App = () => {
-  return(
-    <div>
-        <Button type="primary">Primary Button</Button>
-    </div>
-  )
-}
+const App: FC = () => {
+  return (
+    <>
+    <Layout>
+      <HeaderDict />
+      <div>
+      <Button type="primary">Primary Button</Button>
+      </div>
+      
+    </Layout>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/HeaderDict/HeaderDict.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Layout } from 'antd';
+
+const { Header } = Layout;
+
+const HeaderDict: FC = () => {
+  return <Header>dictionary</Header>;
+};
+
+export default HeaderDict;

2d440376298a806f360e0d78123dcd70a9d1e726
successfulcode <56683358+successfulcode@users.noreply.github.com>
2021-10-19 23:06:19 +0300

    Installed antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import { Button } from 'antd';
 
-function App() {
-  return <div>SM Dictionary</div>;
+const App = () => {
+  return(
+    <div>
+        <Button type="primary">Primary Button</Button>
+    </div>
+  )
 }
 
 export default App;

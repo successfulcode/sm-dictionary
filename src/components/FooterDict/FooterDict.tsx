dac1e9e4d720a24bda690677ce8e61386940b292
successfulcode <56683358+successfulcode@users.noreply.github.com>
2021-10-19 23:42:01 +0300

    App to separate folder

---
+++ src/components/FooterDict/FooterDict.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
+
+const FooterDict: FC = () => {
+  return <Footer>Footer</Footer>;
+};
+
+export default FooterDict;
